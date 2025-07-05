89435ae72b7cedaea6716b0c3cac87f4718a1a76
BoreilFobs <fobsboreil@gmail.com>
2025-07-06 00:43:28 +0100

    register fix

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -158,11 +158,10 @@ export default function AuthScreen() {
         if (response.data.token) {
             await AsyncStorage.setItem('auth_token', response.data.token);
             await AsyncStorage.setItem('user', JSON.stringify(response.data.user));
-            await AsyncStorage.setItem('teacher', JSON.stringify(response.data.teacher));
             await AsyncStorage.setItem('user_id', parseInt(response.data.user.id).toString());
 
             if (isLogin) {
-              
+                await AsyncStorage.setItem('teacher', JSON.stringify(response.data.teacher));
                 router.push('/(tabs)');
             } else {
                 router.push('/setup');
